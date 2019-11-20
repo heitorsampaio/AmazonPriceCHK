2c8130aa77d9951f8643bed35102ce457a3e0aba
Heitor Sampaio <horlando.heitor@gmail.com>
2019-11-20 17:49:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests 
+from bs4 import BeautifulSoup
+import smtplib
+import time
+
+URL = input('Insert the Amazon link of desired product: ')
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64;     x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate",     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+def checkPrice():
+    page = requests.get(URL, headers=headers)
+
+    content = page.content
+
+    soup = BeautifulSoup(content, 'lxml')
+    title = soup.find(id="productTitle")
+    price = soup.find(id='priceblock_ourprice')
+    rmvE = price.get_text().replace('€','')
+    converted_price = float(rmvE[0:].replace(',', ''))
+
+if converted_price < 44900 :
+    sendMail()
+
+print(title.get_text().strip())
+print(converted_price)
+
+def sendMail():
+    server = smtplib.SMTP('smtp.live.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    
+    server.login('', '')
+
+    subject = 'Price Fell Down!'
+    body = f'The price is {rmvE}€, check amazon Link: {URL}'
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        '',
+        '',
+        msg 
+    )
+
+    print('Email has been sent!!!')
+    server.quit()
+
+while True:
+    checkPrice()
+    time.sleep(60 * 60)
+
